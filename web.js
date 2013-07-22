52383289e2036eb1d583a42a74046c251c9c17a5
nassim <simopr@hotmail.fr>
2013-07-22 19:04:36 +0000

    Read from index.html

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
 var express = require('express');
 var fs = require("fs");
-var buf = require("Buffer");
-
 var app = express();
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
